28d2263b2783d30a023cfdb0a9b032a1e30be611
fatima galantls <fatimagalant03@gmail.com>
2022-08-25 16:05:49 +0200

    changes

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // Import needed libraries
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
-const router = express();
+const path = require("path");
 require("dotenv").config();
 
 const productRoute = require("./routes/productRoute.js");
 const userRoute = require("./routes/userRoute.js");
 
-router.set("port", process.env.PORT || 3000);
-router.use(express.json());
-router.use(cors());
-router.use(express.static("public"));
 
-// router.use("/index.html", express.static(__dirname + "/index.html"));
-// router.get("/", (req, res) => {
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname,"public")));
+
+// app.use("/index.html", express.static(__dirname + "/index.html"));
+// app.get("/", (req, res) => {
 //   res.sendFile(__dirname + "/" + "index.html");
 // });
 
-router.use("/products", productRoute);
-router.use("/users", userRoute);
-router.listen(router.get("port"), () => {
-  console.log(`Listening for calls on port ${router.get("port")}`);
+app.use("/products", productRoute);
+app.use("/users", userRoute);
+
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening for calls on port ${app.get("port")}`);
   console.log("Press Ctrl+C to exit server");
 });
