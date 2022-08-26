7b15fc7d5480f1230a12ecbbc71c99f8b7f8a22c
fatima galantls <fatimagalant03@gmail.com>
2022-08-26 16:11:05 +0200

    push

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -187,4 +187,18 @@ router.post("/login", (req, res) => {
   }
 });
 
+// VERIFY
+router.get("/users/verify", (req, res) => {
+  const token = req.header("x-auth-token");
+  jwt.verify(token, process.env.jwtSecret, (error, decodedToken) => {
+    if (error) {
+      res.status(401).json({
+        msg: "Unauthorized Access!",
+      });
+    } else {
+      res.status(200);
+      res.send(decodedToken);
+    }
+  });
+});
 module.exports = router;
