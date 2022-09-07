21ae201dff1f0d1318690fc45648c022d719323b
fatima galantls <fatimagalant03@gmail.com>
2022-09-07 13:27:29 +0200

    commit

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,7 +1,7 @@
 const sql = require("mysql2");
 require("dotenv").config();
 
-var con = sql.createConnection({
+var con = sql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
